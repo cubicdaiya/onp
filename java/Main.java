1881f4435d7409a7c3c545cb494bb64ddee99dde
cubicdaiya <cubicdaiya@gmail.com>
2009-09-23 15:14:11 +0900

    add java

---
+++ java/Main.java
@@ -0,0 +1,12 @@
+import onp.Diff;
+
+
+public class Main {
+	public static void main(String[] args) {
+		Integer a[] = {1,2,3,4,5,6,7,8,9,10};
+		Integer b[] = {1,2,3,4,5,0,7,8,9,10};
+		Diff<Integer> diff = new Diff<Integer>(a, b);
+		diff.compose();
+		System.out.println(diff.getEditdis());
+	}
+}
