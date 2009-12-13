b4552e457487302eb34db2040bb76568358a690b
cubicdaiya <cubicdaiya@gmail.com>
2009-12-13 16:22:44 +0900

    add ruby implimentation

---
+++ ruby/editdis.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require "onp.rb"
+
+a = ARGV[0]
+b = ARGV[1]
+
+diff = Onp.new(a, b);
+diff.compose()
+print "editdistance:" + diff.editdis.to_s() + "\n"
+
+
+
---
+++ ruby/onp.rb
@@ -0,0 +1,53 @@
+
+=begin
+The algorithm implemented here is based on "An O(NP) Sequence Comparison Algorithm"                                   
+by described by Sun Wu, Udi Manber and Gene Myers 
+=end
+
+class Onp
+  attr_reader :editdis
+  def initialize(a, b)
+    @a = a
+    @b = b
+    @m = a.length
+    @n = b.length
+    @reverse = false
+    if @m > @n
+      @a, @b = @b, @a
+      @m, @n = @n, @m
+      @reverse = true
+    end
+    @editdis = 0
+  end
+  def compose()
+    offset = @m + 1
+    delta  = @n - @m
+    size   = @m + @m + 3
+    fp     = Array.new(size){|i| -1}
+    p      = -1
+    begin
+      p = p + 1
+      k = -p
+      while k <= delta - 1
+        fp[k+offset] = snake(k, fp[k-1+offset]+1, fp[k+1+offset])
+        k = k + 1
+      end
+      k = delta + p
+      while k >= delta + 1
+        fp[k+offset] = snake(k, fp[k-1+offset]+1, fp[k+1+offset])
+        k = k - 1
+      end
+      fp[delta+offset] = snake(delta, fp[delta-1+offset]+1, fp[delta+1+offset])
+    end while fp[delta+offset] != @n
+    @editdis = delta + 2 * p
+  end
+  def snake(k, p, pp)
+    y = [p, pp].max
+    x = y - k
+    while x < @m and y < @n and @a[x] == @b[y]
+      x = x + 1
+      y = y + 1
+    end
+    return y
+  end
+end
