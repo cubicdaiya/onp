b76e1ec0de13afaa26bf8efea0c2d038a03e722a
cubicdaiya <cubicdaiya@gmail.com>
2010-05-02 21:09:20 +0900

    chagne indent size

--- c/editdistance.c
+++ c/editdistance.c
@@ -3,16 +3,16 @@
 #include "onp.h"
 
 int main (int argc, char *argv[]) {
-  
-  if (argc < 3) {
-    printf("set two arguments\n");
-    return -1;
-  }
-
-  onp_diff_t *diff = onp_alloc_diff(argv[1], argv[2]);
-  onp_compose(diff);
-  printf("editdistance:%d\n", diff->editdis);
-  onp_free_diff(diff);
-  
-  return 0;
+    
+    if (argc < 3) {
+        printf("set two arguments\n");
+        return -1;
+    }
+    
+    onp_diff_t *diff = onp_alloc_diff(argv[1], argv[2]);
+    onp_compose(diff);
+    printf("editdistance:%d\n", diff->editdis);
+    onp_free_diff(diff);
+    
+    return 0;
 }
--- c/onp.h
+++ c/onp.h
@@ -12,12 +12,12 @@ typedef char onp_sequence_t;
 #define onp_seq_len(seq) strlen(seq)
 
 typedef struct {
-  onp_sequence_t *a;
-  onp_sequence_t *b;
-  uint m;
-  uint n;
-  int *path;
-  uint editdis;
+    onp_sequence_t *a;
+    onp_sequence_t *b;
+    uint m;
+    uint n;
+    int *path;
+    uint editdis;
 } onp_diff_t;
 
 onp_diff_t *onp_alloc_diff (onp_sequence_t *a, onp_sequence_t *b);
