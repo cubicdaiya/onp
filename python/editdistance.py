ca14c397dddd12e5a137f69b47c640220efe644f
cubicdaiya <cubicdaiya@gmail.com>
2010-06-30 20:44:13 +0900

    minimize code

--- python/editdistance.py
+++ python/editdistance.py
@@ -2,11 +2,8 @@
 # -*- encoding: utf-8 -*-
 
 import sys
-#sys.path.insert(0, '../')
-from onp import onp
+import onp
 
 if __name__ == "__main__":
-    diff = onp(sys.argv[1], sys.argv[2])
-    diff.compose()
-    print diff.getEditDistance()
+    print onp.editdistance(sys.argv[1], sys.argv[2])
 
--- python/onp.py
+++ python/onp.py
@@ -1,47 +1,38 @@
+"""
+The algorithm implemented here is based on "An O(NP) Sequence Comparison Algorithm"                                   
+by described by Sun Wu, Udi Manber and Gene Myers 
+"""
 
-class onp(object):
-    """
-    The algorithm implemented here is based on "An O(NP) Sequence Comparison Algorithm"                                   
-    by described by Sun Wu, Udi Manber and Gene Myers 
-    """
+import sys
+
+def editdistance(a, b):
+    m = len(a)
+    n = len(b)
+    if m >= n:
+        a, b = b, a
+        m, n = n, m
+    offset = m + 1
+    delta  = n - m
+    size   = m + n + 3
+    fp = [ -1 for idx in range(size) ]
+    p = -1
+    while (True):
+        p = p + 1
+        for k in range(-p, delta, 1):
+            fp[k+offset] = snake(a, b, m, n, k, fp[k-1+offset]+1, fp[k+1+offset])
+        for k in range(delta+p, delta, -1):
+            fp[k+offset] = snake(a, b, m, n, k, fp[k-1+offset]+1, fp[k+1+offset])
+        fp[delta+offset] = snake(a, b, m, n, delta, fp[delta-1+offset]+1, fp[delta+1+offset])
+        if fp[delta+offset] >= n: break
+    return delta + 2 * p
     
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-        self.m = len(a)
-        self.n = len(b)
-        path = []
-        self.editdis = 0
-        if self.m >= self.n:
-            self.a, self.b = self.b, self.a
-            self.m, self.n = self.n, self.m
+def snake(a, b, m, n, k, p, pp):
+    y = max(p, pp)
+    x = y - k
+    while x < m and y < n and a[x] == b[y]:
+        x = x + 1
+        y = y + 1
+    return y
 
-    def getEditDistance(self):
-        return self.editdis
-
-    def snake(self, k, p, pp):
-        y = max(p, pp)
-        x = y - k
-        while x < self.m and y < self.n and self.a[x] == self.b[y]:
-            x = x + 1
-            y = y + 1
-        return y
-        
-    def compose(self):
-        offset = self.m + 1
-        delta  = self.n - self.m
-        size   = self.m + self.n + 3
-        fp = [ -1 for idx in range(size) ]
-        p = -1
-        while (True):
-            p = p + 1
-            for k in range(-p, delta, 1):
-                fp[k+offset] = self.snake(k, fp[k-1+offset]+1, fp[k+1+offset])
-                
-            for k in range(delta+p, delta, -1):
-                fp[k+offset] = self.snake(k, fp[k-1+offset]+1, fp[k+1+offset])
-
-            fp[delta+offset] = self.snake(delta, fp[delta-1+offset]+1, fp[delta+1+offset])
-            if fp[delta+offset] >= self.n: break
-        self.editdis = delta + 2 * p
-
+if __name__ == "__main__":
+    print editdistance(sys.argv[1], sys.argv[2])
