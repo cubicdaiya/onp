e6861e4b099d7091007c6dc72b8fec24772fcb94
cubicdaiya <cubicdaiya@gmail.com>
2011-01-16 01:46:08 +0900

    adjust space

--- java/Main.java
+++ java/Main.java
@@ -1,12 +1,11 @@
 import onp.Diff;
 
-
 public class Main {
-	public static void main(String[] args) {
-		Integer a[] = {1,2,3,4,5,6,7,8,9,10};
-		Integer b[] = {1,2,3,4,5,0,7,8,9,10};
-		Diff<Integer> diff = new Diff<Integer>(a, b);
-		diff.compose();
-		System.out.println("editdistance:" + diff.getEditdis());
-	}
+    public static void main(String[] args) {
+        Integer a[] = {1,2,3,4,5,6,7,8,9,10};
+        Integer b[] = {1,2,3,4,5,0,7,8,9,10};
+        Diff<Integer> diff = new Diff<Integer>(a, b);
+        diff.compose();
+        System.out.println("editdistance:" + diff.getEditdis());
+    }
 }
--- java/onp/Diff.java
+++ java/onp/Diff.java
@@ -6,57 +6,57 @@ import java.util.Arrays;
  * by described by Sun Wu, Udi Manber and Gene Myers
  */
 public class Diff<T> {
-	
-	private T A[];
-	private T B[];
-	private int M;
-	private int N;
-	private int editdis;
-		
-	public Diff (T a[], T b[]) {
-		this.A = a;
-		this.B = b;
-		this.M = a.length;
-		this.N = b.length;
-		this.editdis = 0;
-		if (this.M > this.N) {
-			T tmp[] = this.A;
-			this.A = this.B;
-			this.B = tmp;
-		}
-	}
-	
-	public int getEditdis () {
-		return this.editdis;
-	}
-	
-	public void compose () {
-		int p = -1;
-		int size = this.M + this.N + 3;
-		int delta = this.N - this.M;
-		int offset = M + 1;
-		int fp[] = new int[size];
-		Arrays.fill(fp, -1);
-		do {
-			++p;
-			for (int k=-p;k<=delta-1;++k) {
-				fp[k+offset] = this.snake(k, fp[k-1+offset]+1, fp[k+1+offset]);
-			}
-			for (int k=delta+p;k>=delta+1;--k) {
-				fp[k+offset] = this.snake(k, fp[k-1+offset]+1, fp[k+1+offset]);
-			}
-			fp[delta+offset] = this.snake(delta, fp[delta-1+offset]+1, fp[delta+1+offset]);
-		} while(fp[delta+offset] < this.N);
-		this.editdis = delta + 2 * p;
-	}
-	
-	private int snake (int k, int p, int pp) {
-		int y = Math.max(p, pp);
-		int x = y - k;
-		while (x < M && y < N && this.A[x] == this.B[y]) {
-			++x;
-			++y;
-		}
-		return y;
-	}
+    
+    private T A[];
+    private T B[];
+    private int M;
+    private int N;
+    private int editdis;
+    
+    public Diff (T a[], T b[]) {
+        this.A = a;
+        this.B = b;
+        this.M = a.length;
+        this.N = b.length;
+        this.editdis = 0;
+        if (this.M > this.N) {
+            T tmp[] = this.A;
+            this.A  = this.B;
+            this.B  = tmp;
+        }
+    }
+    
+    public int getEditdis () {
+        return this.editdis;
+    }
+    
+    public void compose () {
+        int p      = -1;
+        int size   = this.M + this.N + 3;
+        int delta  = this.N - this.M;
+        int offset = M + 1;
+        int fp[]   = new int[size];
+        Arrays.fill(fp, -1);
+        do {
+            ++p;
+            for (int k=-p;k<=delta-1;++k) {
+                fp[k+offset] = this.snake(k, fp[k-1+offset]+1, fp[k+1+offset]);
+            }
+            for (int k=delta+p;k>=delta+1;--k) {
+                fp[k+offset] = this.snake(k, fp[k-1+offset]+1, fp[k+1+offset]);
+            }
+            fp[delta+offset] = this.snake(delta, fp[delta-1+offset]+1, fp[delta+1+offset]);
+        } while(fp[delta+offset] < this.N);
+        this.editdis = delta + 2 * p;
+    }
+    
+    private int snake (int k, int p, int pp) {
+        int y = Math.max(p, pp);
+        int x = y - k;
+        while (x < M && y < N && this.A[x] == this.B[y]) {
+            ++x;
+            ++y;
+        }
+        return y;
+    }
 }
