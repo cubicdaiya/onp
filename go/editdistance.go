be5d54c4cb1841177e73e1d8fd067e15c7f3daf8
bokko <bokko@pixiv.co.jp>
2011-04-03 15:05:37 +0900

    remove semi-colon

--- go/editdistance.go
+++ go/editdistance.go
@@ -6,71 +6,71 @@
 package main
 
 import (
-    "os";
-    "fmt";
+    "os"
+    "fmt"
 )
 
 type diff_info struct {
-    a string;
-    b string;
-    m int;
-    n int;
+    a string
+    b string
+    m int
+    n int
 }
 
 func max (a int, b int) int {
     if a > b {
         return a
     }
-    return b;
+    return b
 }
 
 func snake(k int, p int, pp int, diff *diff_info) int {
-    var y int = max(p, pp);
-    var x int = y - k;
+    var y int = max(p, pp)
+    var x int = y - k
     for x < diff.m && y < diff.n && diff.a[x] == diff.b[y] {
-        x++;
-        y++;
+        x++
+        y++
     }
-    return y;  
+    return y  
 }
 
 func editdis(a string, b string) int {
-    var diff diff_info;
-    diff.a = a;
-    diff.b = b;
-    diff.m = len(a);
-    diff.n = len(b);
+    var diff diff_info
+    diff.a = a
+    diff.b = b
+    diff.m = len(a)
+    diff.n = len(b)
     if diff.m > diff.n {
-        diff.a, diff.b = diff.b, diff.a;
-        diff.m, diff.n = diff.n, diff.m;
+        diff.a, diff.b = diff.b, diff.a
+        diff.m, diff.n = diff.n, diff.m
     }
-    var offset int = diff.m + 1;
-    var delta  int = diff.n - diff.m;
-    var size   int = diff.m + diff.n + 3;
-    var fp     []int = make([]int, size);
+    var offset int = diff.m + 1
+    var delta  int = diff.n - diff.m
+    var size   int = diff.m + diff.n + 3
+    var fp     []int = make([]int, size)
     for i:=0;i<size;i++ {
-        fp[i] = -1;
+        fp[i] = -1
     }
-    var p int = -1;
+    var p int = -1
     for {
-        p++;
+        p++
         for k:=-p;k<=delta-1;k++ {
-            fp[k+offset] = snake(k, fp[k-1+offset]+1, fp[k+1+offset], &diff);
+            fp[k+offset] = snake(k, fp[k-1+offset]+1, fp[k+1+offset], &diff)
         }
         for k:=delta+p;k>=delta+1;k-- {
-            fp[k+offset] = snake(k, fp[k-1+offset]+1, fp[k+1+offset], &diff);
+            fp[k+offset] = snake(k, fp[k-1+offset]+1, fp[k+1+offset], &diff)
         }
-        fp[delta+offset] = snake(delta, fp[delta-1+offset]+1, fp[delta+1+offset], &diff);
+        fp[delta+offset] = snake(delta, fp[delta-1+offset]+1, fp[delta+1+offset], &diff)
         if fp[delta+offset] >= diff.n {
             break
         }
     }
-    return delta + 2 * p;
+    return delta + 2 * p
 }
 
 func main(){
     if len(os.Args) < 3 {
-        fmt.Printf("set two args\n");
+        fmt.Printf("set two args\n")
     } else {
         fmt.Printf("editdistance:%d\n", editdis(os.Args[1], os.Args[2]))
     }
