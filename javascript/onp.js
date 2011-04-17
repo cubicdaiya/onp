6c603726278420b1f64d74a86b9173dc5aaa3a64
bokko <bokko@pixiv.co.jp>
2011-04-17 23:18:46 +0900

    modified description

--- javascript/onp.js
+++ javascript/onp.js
@@ -1,5 +1,4 @@
 /**
- * compose Longest Common Subsequence and Shortest Edit Script.
  * The algorithm implemented here is based on "An O(NP) Sequence Comparison Algorithm"
  * by described by Sun Wu, Udi Manber and Gene Myers
 */
