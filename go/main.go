668be18bd32d133183486673783028edec6ef7e8
cubicdaiya <cubicdaiya@gmail.com>
2013-08-26 00:59:36 +0900

    use runtime.GOMAXPROCS

--- go/main.go
+++ go/main.go
@@ -4,12 +4,15 @@ import (
 	"./onp"
 	"fmt"
 	"os"
+	"runtime"
 )
 
 func main() {
 	if len(os.Args) < 3 {
 		fmt.Printf("set two args\n")
 	} else {
+		cpus := runtime.NumCPU()
+		runtime.GOMAXPROCS(cpus)
 		diff := onp.NewDiff(os.Args[1], os.Args[2])
 		fmt.Printf("editdistance:%d\n", diff.EditDistance())
 	}
