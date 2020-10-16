bda6b9b25aa4770de1520edbd540f96b55c912c8
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2020-10-17 06:25:34 +0900

    add rust implementation.

---
+++ rust/main.rs
@@ -0,0 +1,95 @@
+use std::cmp::max;
+use std::env::args;
+
+struct Diff {
+    a: String,
+    b: String,
+    m: usize,
+    n: usize,
+}
+
+fn build_diff(a: String, b: String) -> Diff {
+    if a.len() > b.len() {
+        return Diff {
+            a: b.to_string(),
+            b: a.to_string(),
+            m: b.len(),
+            n: a.len(),
+        };
+    }
+    return Diff {
+        a: a.to_string(),
+        b: b.to_string(),
+        m: a.len(),
+        n: b.len(),
+    };
+}
+
+impl Diff {
+    fn snake(&self, mut x: usize, mut y: usize) -> usize {
+        while x < self.m
+            && y < self.n
+            && self.a.chars().nth(x).unwrap() == self.b.chars().nth(y).unwrap()
+        {
+            x = x + 1;
+            y = y + 1;
+        }
+
+        return y;
+    }
+
+    fn ed(&self) -> usize {
+        let offset: usize = self.m + 1;
+        let delta: usize = self.n - self.m;
+        let size = self.m + self.n + 3;
+        let mut fp: Vec<usize> = vec![0; size];
+
+        let mut p: usize = 0;
+        loop {
+            let mut k = offset - p;
+            while k <= delta + offset - 1 {
+                let y = max(fp[k - 1] + 1, fp[k + 1]);
+                if offset < k {
+                    fp[k] = self.snake(y - (k - offset), y);
+                } else {
+                    fp[k] = self.snake(y + (offset - k), y);
+                }
+                k = k + 1;
+            }
+            let mut l = delta + p;
+            while l >= delta + 1 {
+                let y = max(fp[l + offset - 1] + 1, fp[l + 1 + offset]);
+                fp[l + offset] = self.snake(y - l, y);
+                l = l - 1;
+            }
+
+            let y = max(fp[delta + offset - 1] + 1, fp[delta + 1 + offset]);
+            fp[delta + offset] = self.snake(y - delta, y);
+
+            if fp[delta + offset] >= self.n {
+                break;
+            }
+            p = p + 1;
+        }
+        return delta + 2 * p;
+    }
+}
+
+fn main() {
+    let args: Vec<String> = args().collect();
+
+    if args.len() < 3 {
+        println!("few arguments");
+        std::process::exit(1);
+    }
+
+    let a = &args[1];
+    let b = &args[2];
+
+    println!("{}", a);
+    println!("{}", b);
+
+    let d = build_diff(a.to_string(), b.to_string());
+
+    println!("ed: {}", d.ed());
+}
