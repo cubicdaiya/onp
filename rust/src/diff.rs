8ef1aafd8da2f38319d557bb67e4d53e563da3ea
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2022-04-14 10:33:29 +0900

    rust: fixed invalid calculation.

--- rust/src/diff.rs
+++ rust/src/diff.rs
@@ -22,10 +22,12 @@ pub fn build_diff(mut a: String, mut b: String) -> Diff {
 }
 
 impl Diff {
-    fn snake(&self, mut x: usize, mut y: usize) -> usize {
-        while x < self.m
-            && y < self.n
-            && self.a.chars().nth(x).unwrap() == self.b.chars().nth(y).unwrap()
+    fn snake(&self, mut x: isize, mut y: isize) -> isize {
+        while x >= 0
+            && y >= 0
+            && x < self.m as isize
+            && y < self.n as isize
+            && self.a.chars().nth(x as usize).unwrap() == self.b.chars().nth(y as usize).unwrap()
         {
             x = x + 1;
             y = y + 1;
@@ -38,27 +40,27 @@ impl Diff {
         let offset: usize = self.m + 1;
         let delta: usize = self.n - self.m;
         let size = self.m + self.n + 3;
-        let mut fp: Vec<usize> = vec![0; size];
+        let mut fp: Vec<isize> = vec![-1; size];
 
         let mut p: usize = 0;
         loop {
             let mut k = offset - p;
             while k <= delta + offset - 1 {
                 let y = max(fp[k - 1] + 1, fp[k + 1]);
-                fp[k] = self.snake(y + offset - k, y);
+                fp[k] = self.snake(y - (k as isize) + (offset as isize), y);
                 k = k + 1;
             }
             let mut l = delta + p;
             while l >= delta + 1 {
                 let y = max(fp[l + offset - 1] + 1, fp[l + 1 + offset]);
-                fp[l + offset] = self.snake(y - l, y);
+                fp[l + offset] = self.snake(y - (l as isize), y);
                 l = l - 1;
             }
 
             let y = max(fp[delta + offset - 1] + 1, fp[delta + 1 + offset]);
-            fp[delta + offset] = self.snake(y - delta, y);
+            fp[delta + offset] = self.snake(y - (delta as isize), y);
 
-            if fp[delta + offset] >= self.n {
+            if fp[delta + offset] >= (self.n as isize) {
                 break;
             }
             p = p + 1;
